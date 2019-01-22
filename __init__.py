38b89e45f22b3ec6de327ca09e273038495707a3
MregXN <MregXN@gmail.com>
2019-01-22 22:35:32 +0800

    fix some bug and modify the __init__.py

--- __init__.py
+++ __init__.py
@@ -12,5 +12,19 @@ if __name__ == "__main__":
             print(scanThunderboards())
             print("input mac addr to connect the devices")
             x = input()
+            print("input addr type of yuor device")
+            y= input()
+            manager.addBoard(x, y)
+            thunderboardData = dict()
+            for index, obj in enumerate(manager.list):
+                if obj["mac_address"] == x:
+                    thunderboardData['Acceleration_x'] = manager.list[index]['handle'].thunderboardData['Acceleration_x']
+                    thunderboardData['Acceleration_y'] = manager.list[index]['handle'].thunderboardData['Acceleration_y']
+                    thunderboardData['Acceleration_z'] = manager.list[index]['handle'].thunderboardData['Acceleration_z']
+                    thunderboardData['Orientation_x'] = manager.list[index]['handle'].thunderboardData['Orientation_x']
+                    thunderboardData['Orientation_y'] = manager.list[index]['handle'].thunderboardData['Orientation_y']
+                    thunderboardData['Orientation_z'] = manager.list[index]['handle'].thunderboardData['Orientation_z']
+            print(thunderboardData)
+
         else:
             print("please input again")
